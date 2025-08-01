204855bfd8cc31a58cc67e41f82f9b4cd69cea47
skosdelhi <shreekssdelhi@gmail.com>
2025-08-01 18:57:18 +0530

    Enable PostgreSQL config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://org_user:khirod123@localhost/social_org_db")
+#SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://org_user:khirod123@localhost/social_org_db")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://skos_db_user:8djNQZNWXXNgFx7RUgrQQrKYnd0oyev5@dpg-d26bpkali9vc73d4bhg0-a/skos_db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "super-secret-key"
